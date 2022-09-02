87f1b03417527108c72d712673cabde76d95da3b
Mahir Absar Bin Mohsin <mahir.mohsin@sazim.io>
2022-09-02 04:37:25 -0700

    Install libraries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,20 @@
-import logo from "./logo.svg";
-import { useEffect } from "react";
 import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage/HomePage";
+import SignUpPage from "./Pages/SignUpPage/SignUpPage";
+import LoginPage from "./Pages/LoginPage/LoginPage";
 
 function App() {
-  useEffect(() => {
-    fetch("/api/")
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
-  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="sign-up" element={<SignUpPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
