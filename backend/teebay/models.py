f4ed3f56afa0fdc491ba2411990bd6b61a874146
Mahir Absar Bin Mohsin <mahir.mohsin@sazim.io>
2022-09-03 08:39:06 -0700

    Create product model

--- backend/teebay/models.py
+++ backend/teebay/models.py
@@ -1,5 +1,6 @@
+from enum import unique
 from teebay import db
-
+from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     publicId = db.Column(db.String(500),nullable=False)
@@ -13,6 +14,20 @@ class User(db.Model):
 
     def __repr__(self):
         return f'{self.email}'
+
+class Product(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(200),unique=True)
+    categories = db.Column(db.String(100),nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Float,nullable=False)
+    rentPrice = db.Column(db.Float,nullable=False)
+    rentType = db.Column(db.String(20),nullable=False)
+
+    def __repr__(self):
+        return f'{self.title}'
+
+
 
 
     
--- backend/teebay/views.py
+++ backend/teebay/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint,jsonify
+from cmath import exp
+from flask import Blueprint,jsonify,request
 from .auth import tokenRequired
 views = Blueprint("views",__name__)
 
@@ -9,5 +10,16 @@ def home():
         return jsonify({"message":"hello world"})
     except Exception as e:
         return jsonify({"message":"Not Authorized"})
+
+@views.route("/products",methods=["GET","POST"])
+@tokenRequired
+def getAddProduct(loginUser,*args,**kwargs):
+    if request.method == "POST":
+        try:
+            pass
+        except Exception as e:
+            return jsonify({"message":str(e)})
+    return jsonify({"message":"GET REQ"})
+
 
         
