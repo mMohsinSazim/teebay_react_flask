aeb95bdbd3b172beb3d8ad7145420b7ede74c83f
Mahir Absar Bin Mohsin <mahir.mohsin@sazim.io>
2022-09-05 03:39:55 -0700

    Connect with postgres database

--- backend/teebay/__init__.py
+++ backend/teebay/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "634d417b55f940be9b86ad65688d7b30"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///teebay.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://@localhost/teebay_postgres"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
